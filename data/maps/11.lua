4dbd9f3e09bb0eb6a7b8ee1eebee99f1ff6aa9ee
wrightmat <wright.matt.a@gmail.com>
2014-04-06 13:32:50 -0500

    Zirna, the dark enchantress

--- data/maps/11.lua
+++ data/maps/11.lua
@@ -269,3 +269,28 @@ end
 function torch_3:on_interaction_item(lamp)
   torch_3:get_sprite():set_animation("lit")
 end
+
+function ocarina_wind_to_D7:on_interaction()
+  -- if this point not previously discovered
+  -- then add it, otherwise do nothing
+  if not game:get_value("b1502") then
+    game:start_dialog("warp.new_point", function()
+      game:set_value("b1502", true)
+    end)
+  else
+    -- if other paired point is discovered (and they have the Ocarina),
+    -- then ask the player if they want to warp there!
+   if game:has_item("ocarina") then
+    if game:get_value("b1503") then
+      game:start_dialog("warp.to_D7", function(answer)
+        if answer == 1 then
+          sol.audio.play_sound("ocarina_wind")
+          map:get_entity("hero"):teleport(51, "ocarina_warp", "fade")
+        end
+      end)
+    else
+      game:start_dialog("warp.interaction")
+    end
+   end
+  end
+end
--- data/maps/51.lua
+++ data/maps/51.lua
@@ -55,3 +55,28 @@ function game:on_map_changed(map)
     if map:get_world() ~= "inside_world" then sunset_overlay:draw(dst_surface) end
   end
 end
+
+function ocarina_wind_to_F14:on_interaction()
+  -- if this point not previously discovered
+  -- then add it, otherwise do nothing
+  if not game:get_value("b1503") then
+    game:start_dialog("warp.new_point", function()
+      game:set_value("b1503", true)
+    end)
+  else
+    -- if other paired point is discovered (and they have the Ocarina),
+    -- then ask the player if they want to warp there!
+   if game:has_item("ocarina") then
+    if game:get_value("b1502") then
+      game:start_dialog("warp.to_F14", function(answer)
+        if answer == 1 then
+          sol.audio.play_sound("ocarina_wind")
+          map:get_entity("hero"):teleport(11, "ocarina_warp", "fade")
+        end
+      end)
+    else
+      game:start_dialog("warp.interaction")
+    end
+   end
+  end
+end
