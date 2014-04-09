786390999098a6aac0a06a2d2a954c6fd76afa45
wrightmat <wright.matt.a@gmail.com>
2014-04-08 23:18:46 -0500

    Fire keese behavior

--- data/enemies/keese_fire.lua
+++ data/enemies/keese_fire.lua
@@ -38,21 +38,11 @@ function enemy:check_hero()
     and self:get_distance(hero) < 100
 
   if near_hero and not going_hero then
-    self:go_hero()
+    self:go_circle()
   elseif not near_hero and going_hero then
     self:go_random()
-  elseif not near_hero and not going_hero then
-    self:go_circle()
   end
   timer = sol.timer.start(self, 2000, function() self:check_hero() end)
-end
-
-function enemy:go_hero()
-  self:get_sprite():set_animation("walking")
-  local m = sol.movement.create("target")
-  m:set_speed(64)
-  m:start(self)
-  going_hero = true
 end
 
 function enemy:go_random()
