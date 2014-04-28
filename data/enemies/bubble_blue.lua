14af488312afbe5bda26704e1be5e975fea46e46
wrightmat <wright.matt.a@gmail.com>
2014-04-27 22:20:02 -0500

    Better chuchus

--- data/enemies/bubble_blue.lua
+++ data/enemies/bubble_blue.lua
@@ -7,7 +7,7 @@ local last_direction8 = 0
 
 function enemy:on_created()
   self:set_life(1)
-  self:create_sprite("enemies/bubble")
+  self:create_sprite("enemies/bubble_blue")
   self:set_size(8, 8)
   self:set_origin(4, 4)
   self:set_can_hurt_hero_running(true)
@@ -60,10 +60,5 @@ function enemy:on_attacking_hero(hero)
 
   -- In any case, we do the hurt animation as usual
   hero:start_hurt(enemy, 2)
-
-  -- If hero has magic, it is drained.
-  if game:get_magic() > 0 then
-    game:remove_magic(4)
-    sol.audio.play_sound("magic_bar")
-  end
+  sol.audio.play_sound("splash")
 end
