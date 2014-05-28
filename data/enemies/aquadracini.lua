3f03156dba06c8b7b76f2f834b9ad54aee90ef88
wrightmat <wright.matt.a@gmail.com>
2014-05-27 21:49:21 -0500

    Aquadracini shouldn't have that much life

--- data/enemies/aquadracini.lua
+++ data/enemies/aquadracini.lua
@@ -6,7 +6,7 @@ local going_hero = false
 local timer
 
 function enemy:on_created()
-  self:set_life(6)
+  self:set_life(2)
   self:set_damage(2)
   self:create_sprite("enemies/aquadracini")
   self:set_hurt_style("monster")
