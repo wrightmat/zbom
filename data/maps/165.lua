7442d311be5f5d5ac72d1371230eeb22603f750c
wrightmat <wright.matt.a@gmail.com>
2014-06-07 20:40:42 -0500

    Great fairies

--- data/maps/165.lua
+++ data/maps/165.lua
@@ -1,13 +1,35 @@
 local map = ...
+local game = map:get_game()
 
----------------------------------------------------
--- Great Fairy Fountain of Courage - Faron Woods --
----------------------------------------------------
+-----------------------------------------------
+-- Great Fairy Fountain of Wisdom - Snowpeak --
+-----------------------------------------------
+
+if game:get_value("i1605")==nil then game:set_value("i1605", 0) end
+if game:get_value("i1830")==nil then game:set_value("i1830", 0) end
 
 function map:on_started(destination)
-
+  if game:get_value("i1830") >= 25 then game:set_value("i1605", 2) end
+  if game:get_value("i1830") >= 50 then game:set_value("i1605", 3) end
+  if game:get_value("i1830") >= 75 then game:set_value("i1605", 4) end
+  if game:get_value("i1830") == 99 then game:set_value("i1605", 5) end
 end
 
-function npc_great_fairy:on_interaction()
-
+function sensor_fairy_speak:on_activated()
+  if game:get_value("i1605") == 1 then
+    game:start_dialog("great_fairy.1")
+  elseif game:get_value("i1605") == 2 then
+    game:start_dialog("great_fairy.2")
+  elseif game:get_value("i1605") == 3 then
+    game:start_dialog("great_fairy.3")
+  elseif game:get_value("i1605") == 4 then
+    game:start_dialog("great_fairy.4")
+  elseif game:get_value("i1605") == 5 then
+    game:start_dialog("great_fairy.5.mountain")
+  else
+    game:start_dialog("great_fairy.0.mountain", function()
+      game:set_value("i1605", 1)
+    end)
+  end
+  sensor_fairy_speak:set_enabled(false)
 end
