42067b2570008475ad3a8cb8a530217d0b8aa10e
wrightmat <wright.matt.a@gmail.com>
2014-06-18 21:30:37 -0500

    Split Chu into two parts (base and head)

--- data/enemies/chu_big_ice.lua
+++ data/enemies/chu_big_ice.lua
@@ -3,23 +3,28 @@ local enemy = ...
 -- Big Ice Chu: A large gelatinous miniboss who
 -- tries to either squish or freeze our hero.
 
-local going_hero = false
-local being_pushed = false
-local base_sprite = nil
-local head_sprite = nil
-local timer
+local head = nil
+local current_xy = {}
 
 function enemy:on_created()
   self:set_life(10)
   self:set_damage(2)
-  head_sprite = self:create_sprite("enemies/chu_big_ice")
-  base_sprite = self:create_sprite("enemies/chu_big_ice_base")
-  self:set_hurt_style("boss")
+  self:create_sprite("enemies/enemies/chu_big_ice")
   self:set_size(48, 48)
   self:set_origin(24, 43)
+  self:set_hurt_style("boss")
+  self:set_pushed_back_when_hurt(false)
+  self:set_push_hero_on_sword(true)
 
-  self:set_invincible_sprite(head_sprite)
-  self:set_attack_consequence_sprite(head_sprite, "sword", "custom")
+  -- Create the head.
+  local my_name = self:get_name()
+  head = self:create_enemy{
+    name = my_name .. "_head",
+    breed = "chu_big_ice_head",
+    x = 0,
+    y = -112
+  }
+  head.base = self
 end
 
 function enemy:on_restarted()
@@ -31,12 +36,43 @@ function enemy:on_restarted()
       self:check_hero()
     end
   end
+  current_xy.x, current_xy.y = self:get_position()
 end
 
 function enemy:on_hurt()
   if timer ~= nil then
     timer:stop()
     timer = nil
+  end
+end
+
+function enemy:on_movement_finished(movement)
+  if being_pushed then
+    self:go_hero()
+  end
+end
+
+function enemy:on_obstacle_reached(movement)
+  if being_pushed then
+    self:go_hero()
+  end
+end
+
+function enemy:on_position_changed(x, y)
+  if head:exists() then
+    -- The base has just moved: do the same movement to the head.
+    local dx = x - current_xy.x
+    local dy = y - current_xy.y
+    local head_x, head_y = head:get_position()
+    head:set_position(head_x + dx, head_y + dy)
+  end
+  current_xy.x, current_xy.y = x, y
+end
+
+function enemy:on_pre_draw()
+  if head:exists() then
+    local x, y = self:get_position()
+    local head_x, head_y = head:get_position()
   end
 end
 
@@ -53,34 +89,6 @@ function enemy:check_hero()
     self:go_random()
   end
   timer = sol.timer.start(self, 1000, function() self:check_hero() end)
-end
-
-function enemy:on_movement_changed(movement)
-  if not being_pushed then
-    local direction4 = movement:get_direction4()
-    main_sprite:set_direction(direction4)
-    sword_sprite:set_direction(direction4)
-  end
-end
-
-function enemy:on_movement_finished(movement)
-  if being_pushed then
-    self:go_hero()
-  end
-end
-
-function enemy:on_obstacle_reached(movement)
-  if being_pushed then
-    self:go_hero()
-  end
-end
-
-function enemy:on_custom_attack_received(attack, sprite)
-  if attack == "sword" and sprite == head_sprite then
-    being_pushed = true
-    head_sprite:set_animation("hurt")
-    timer = sol.timer.start(self, 5000, function() self:go_hero() end)
-  end
 end
 
 function enemy:go_random()
