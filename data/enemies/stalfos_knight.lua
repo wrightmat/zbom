4354716a54384299beeba95164211f1c8774f6e1
wrightmat <wright.matt.a@gmail.com>
2015-02-05 18:47:55 -0600

    Working on Snowpeak and Stalfos Knight, untested

--- data/enemies/stalfos_knight.lua
+++ data/enemies/stalfos_knight.lua
@@ -1,42 +1,71 @@
 local enemy = ...
+local map = enemy:get_map()
 
 -- Stalfos: An undead soldier boss. PLACEHOLDER!
 
 -- Possible positions where he lands.
 local positions = {
-  {x = 224, y = 288 },
-  {x = 232, y = 192 },
-  {x = 360, y = 304 },
-  {x = 336, y = 184 },
-  {x = 288, y = 256 }
+  {x = 224, y = 288, direction4 = 3},
+  {x = 232, y = 192, direction4 = 3},
+  {x = 360, y = 304, direction4 = 3},
+  {x = 336, y = 184, direction4 = 3},
+  {x = 288, y = 256, direction4 = 3}
 }
+
+local vulnerable = false
+local hidden = false
+local timers = {}
 
 function enemy:on_created()
   self:set_life(10)
   self:set_damage(4)
   self:create_sprite("enemies/stalfos_knight")
-  self:set_hurt_style("boss")
-  self:set_pushed_back_when_hurt(false)
-  self:set_push_hero_on_sword(false)
   self:set_size(32, 40)
   self:set_origin(16, 36)
+  self:set_hurt_style("boss")
+  self:set_attack_consequence("arrow", "protected")
+  self:set_attack_consequence("hookshot", "protected")
+  self:set_attack_consequence("boomerang", "protected")
+  self:set_attack_consequence("explosion", "custom")
+  self:set_attack_consequence("sword", "protected")
+  self:set_pushed_back_when_hurt(false)
+  self:set_push_hero_on_sword(false)
 end
 
-function enemy:on_movement_changed(movement)
-  local direction4 = movement:get_direction4()
+function enemy:on_restarted()
+  for _, t in ipairs(timers) do t:stop() end
   local sprite = self:get_sprite()
-  sprite:set_direction(direction4)
+  local action = math.random(2)
+
+  if hidden == false then
+    if action == 1 then
+      timers[#timers + 1] = sol.timer.start(self, math.random(10)*700, function() self:hide() end)
+    else
+      self:go_hero()
+    end
+  end
 end
 
 function enemy:on_obstacle_reached(movement)
   enemy:restart()
 end
-
-function enemy:on_restarted()
-  --Main action
-end
 function enemy:on_hurt()
   enemy:restart()
+end
+
+function enemy:hide()
+  vulnerable = false
+  hidden = true
+  self:set_position(-100, -100)
+  timers[#timers + 1] = sol.timer.start(self, math.random(10)*500, function() self:unhide() end)
+end
+
+function enemy:unhide()
+  hidden = false
+  local position = (positions[math.random(#positions)])
+  self:set_position(position.x, position.y)
+  local sprite = self:get_sprite()
+  sprite:set_direction(position.direction4)
 end
 
 function enemy:go_hero()
@@ -45,4 +74,21 @@ function enemy:go_hero()
   m:set_speed(64)
   m:start(self)
   sol.timer.start(enemy, math.random(10)*1000, function() enemy:restart() end)
+end
+
+function enemy:on_custom_attack_received(attack, sprite)
+  if attack == "explosion" then
+    vulnerable = true
+    self:get_sprite():set_animation("immobilized")
+    self:set_attack_consequence("sword", 1)
+    attack_timer = sol.timer.start(self, 3000, function()
+      vulnerable = false
+      self:get_sprite():set_animation("shaking")
+      self:set_attack_consequence("sword", "protected")
+    end)
+  end
+
+  function sprite:on_animation_finished(animation)
+    if animation == "shaking" then self:get_sprite():set_animation("walking") end
+  end
 end
--- data/maps/209.lua
+++ data/maps/209.lua
@@ -1,12 +1,47 @@
 local map = ...
 local game = map:get_game()
+local i = 0
+local state = 1
 
 -------------------------------------------
 -- Dungeon 5: Snowpeak Caverns (Floor 2) --
 -------------------------------------------
 
+-- initial positions for pits (before star switch activated)
+local pit_positions_boss_1 = {
+  {x = 480, y = 264},
+  {x = 584, y = 344},
+  {x = 696, y = 280},
+  {x = 880, y = 240},
+  {x = 800, y = 104},
+  {x = 768, y = 864}
+}
+-- new positions for pits (after star switch activated)
+local pit_positions_boss_2 = {
+  {x = 512, y = 288},
+  {x = 704, y = 256},
+  {x = 768, y = 168},
+  {x = 912, y = 64},
+  {x = 816, y = 368},
+  {x = 880, y = 864}
+}
+
 function map:on_started(destination)
   boss_stalfos:set_enabled(false)
+end
+
+function switch_star_boss:on_activated()
+  for entity in map:get_entities("hole_boss") do
+    i = i + 1
+    if state == 2 then
+      local position = (pit_positions_boss_2[i])
+      state = 1
+    else
+      local position = (pit_positions_boss_1[i])
+      state = 2
+    end
+    entity:set_position(position.x, position.y)
+  end
 end
 
 function sensor_boss:on_activated()
