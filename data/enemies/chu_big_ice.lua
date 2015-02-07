077b89bf25470de02e7b65e078dd5829a8d59e22
wrightmat <wright.matt.a@gmail.com>
2015-02-07 08:37:01 -0600

    Hopefully finished work on big blue ice chu

--- data/enemies/chu_big_ice.lua
+++ data/enemies/chu_big_ice.lua
@@ -8,19 +8,21 @@ local head = nil
 local current_xy = {}
 local freezing = false
 local going_hero = false
+local freeze_timer = nil
 
 function enemy:on_created()
-  self:set_life(10)
-  self:set_damage(2)
+  self:set_life(8)
+  self:set_damage(4)
   self:create_sprite("enemies/chu_big_ice")
   self:set_size(48, 48)
   self:set_origin(24, 43)
   self:set_hurt_style("boss")
   self:set_pushed_back_when_hurt(false)
   self:set_push_hero_on_sword(true)
-end
+  self:set_attack_consequence("explosion", "custom")
+--end
 
-function enemy:on_enabled()
+--function enemy:on_enabled()
   -- Create the head.
   local my_name = self:get_name()
   head = self:create_enemy{
@@ -34,12 +36,15 @@ end
 
 function enemy:on_restarted()
   if not freezing then
+    print('restarted: not freezing')
     if going_hero then
-      self:go_hero()
+      self:freeze()
     else
       self:go_random()
       self:check_hero()
     end
+  else
+    print('restarted: freezing')
   end
   current_xy.x, current_xy.y = self:get_position()
 end
@@ -53,7 +58,7 @@ function enemy:on_obstacle_reached(movement)
 end
 
 function enemy:on_position_changed(x, y)
-  if head:exists() then
+  if head ~= nil then
     -- The base has just moved: do the same movement to the head.
     local dx = x - current_xy.x
     local dy = y - current_xy.y
@@ -66,78 +71,98 @@ end
 function enemy:on_attacking_hero(hero, enemy_sprite)
   -- If the enemy is frozen, then freeze the hero.
   if freezing then
-    if not freeze_timer then map:get_hero():start_frozen(3000) end
-    freeze_timer = sol.timer.start(self, 4000, function() freeze_timer:stop() end)
+    print('freezing the hero on attack')
+    if not freeze_timer then
+      map:get_hero():start_hurt(2)
+      map:get_hero():start_frozen(3000)
+    end
+    freeze_timer = sol.timer.start(self, 4000, function() freeze_timer = nil end)
   end
 end
 
 function enemy:on_hurt(attack)
+  print('hurt')
   -- The head wobbles when hurt.
   head:stop_movement()
   head:get_sprite():set_animation("hurt")
 
+  sol.timer.start(self, 1000, function() self:go_random() end)
+end
+
+function enemy:on_custom_attack_received(attack, sprite)
   -- If the enemy is frozen, then freeze the hero.
   -- A bomb explosion gets rid of the freezing ice.
-  if freezing then
-    if attack == "explosion" or attack == "fire" then
-      freezing = false
-      self:get_sprite():set_animation("walking")
-      self:check_hero()
-    else
-      if not freeze_timer then map:get_hero():start_frozen(3000) end
-      freeze_timer = sol.timer.start(self, 4000, function() freeze_timer:stop() end)
+  if attack == "sword" then
+    print('custom attack received: sword')
+    if not freeze_timer then
+      map:get_hero():start_hurt(2)
+      map:get_hero():start_frozen(3000)
     end
-  else
+    freeze_timer = sol.timer.start(self, 4000, function() freeze_timer = nil end)
+  elseif attack == "explosion" then
+    print('custom attack received: explosion')
     freezing = false
+    sol.audio.play_sound("ice_shatter")
+    self:get_sprite():set_animation("walking")
+    self:set_attack_consequence("sword", 1)
+    self:check_hero()
   end
 end
 
 function enemy:on_dying()
+  print('dying')
   sol.timer.start(self:get_map(), 1000, function()
-    self:get_map():get_entity("miniboss_chu_head"):set_life(0)
+    head:set_life(0)
   end)
 end
 
 function enemy:on_update()
   if freezing then
     self:get_sprite():set_animation("ice")
+    self:set_attack_consequence("sword", "custom")
   else
     self:get_sprite():set_animation("walking")
+    self:set_attack_consequence("sword", 1)
   end
 end
 
 function enemy:check_hero()
-  local hero = self:get_map():get_entity("hero")
-  local _, _, layer = self:get_position()
-  local _, _, hero_layer = hero:get_position()
-  local near_hero = layer == hero_layer
-    and self:get_distance(hero) < 100
+  if not freezing then
+    local hero = self:get_map():get_entity("hero")
+    local _, _, layer = self:get_position()
+    local _, _, hero_layer = hero:get_position()
+    local near_hero = layer == hero_layer
+      and self:get_distance(hero) < 200
 
-  if near_hero then
-    if math.random(3) == 1 then
-      self:freeze()
+    if near_hero then
+      if math.random(2) == 1 then
+	self:create_enemy{
+	  name = "miniboss_chuchu",
+	  breed = "chuchu_blue",
+	  x = -16,
+	  y = -16
+	}
+      else
+        self:go_hero()
+      end
     else
-      self:go_hero()
+      self:go_random()
     end
-  else
-    self:go_random()
+    sol.timer.start(self, 5000, function() self:check_hero() end)
   end
-  sol.timer.start(self, 5000, function() self:check_hero() end)
 end
 
 function enemy:freeze()
+  print('freezing')
   self:stop_movement()
   self:get_sprite():set_animation("ice")
+  self:set_attack_consequence("sword", "custom")
+  self:set_attack_consequence("explosion", "custom")
   freezing = true
-  sol.timer.start(self, 5000, function()
-    freezing = false
-    sol.audio.play_sound("ice_shatter")
-    self:get_sprite():set_animation("walking")
-    self:check_hero()
-  end)
 end
 
 function enemy:go_random()
+  print('going random')
   local movement = sol.movement.create("random_path")
   movement:set_speed(32)
   movement:start(self)
@@ -146,6 +171,7 @@ function enemy:go_random()
 end
 
 function enemy:go_hero()
+  print('going hero')
   local movement = sol.movement.create("target")
   movement:set_speed(40)
   movement:start(self)
--- data/maps/220.lua
+++ data/maps/220.lua
@@ -1,6 +1,4 @@
 local map = ...
 local game = map:get_game()
 
-function map:on_started(destination)
-  hero:teleport("1", "from_intro", "fade")
-end
+
