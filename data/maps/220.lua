8167acab7ced09285c7a0ee92afa44a958881edf
wrightmat <wright.matt.a@gmail.com>
2015-05-22 15:15:48 -0500

    Cleaning up some old resources and adding some new ones

--- data/maps/220.lua
+++ data/maps/220.lua
@@ -1,13 +1,17 @@
 local map = ...
 local game = map:get_game()
+local lines = {}
+
+-- experimenting with creating scrolling credits
 
 function map:on_started()
+  local start = sol.text_surface.create({
+    horizontal_alignment = "center",
+    font = "bom",
+    font_size = 14,
+    text = "CREDITS"
+  })
+  lines[#lines+1] = start
 
-end
-
-function switch:on_activated()
-  print("activated")
-end
-function switch:on_inactivated()
-  print("inactivated")
+  sol.timer.start(1000, add_line)
 end
