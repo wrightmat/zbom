679b8494fdad21d8541da7f8c0f545e126cd9cba
wrightmat <wright.matt.a@gmail.com>
2015-05-22 09:28:10 -0500

    Removing 1.3 backup, adding new Rito sprite and improving Septen

--- data.1.3.bak/items/bottle_4.lua
+++
@@ -1,8 +0,0 @@
-local item = ...
-
-function item:on_created()
-  self:set_assignable(true)
-  self:set_savegame_variable("i1813")
-end
-
-sol.main.load_file("items/bottle")(item)
--- data.1.3.bak/items/trading.lua
+++
@@ -1,9 +0,0 @@
-local item = ...
-
-function item:on_created()
-  self:set_savegame_variable("i1840")
-end
-
-function item:on_obtained(variant, savegame_variable)
-  -- There are 12 variants of this in the trading sequence - do I need to do anything here?
-end
