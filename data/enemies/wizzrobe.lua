7ee09c074501c2c14e377407c06876375717d4fb
wrightmat <wright.matt.a@gmail.com>
2015-06-04 21:45:43 -0500

    A lot more mapping (Wind Tower and Subrosia) plus adding Wizzrobes

---
+++ data/enemies/wizzrobe.lua
@@ -0,0 +1,56 @@
+local enemy = ...
+
+-- Wizzrobe
+
+local vulnerable = false
+local timers = {}
+local ex, ey, el
+
+function enemy:on_created()
+  self:set_life(3)
+  self:set_damage(2)
+  self:create_sprite("enemies/wizzrobe")
+  self:set_optimization_distance(0)
+  self:set_size(16, 24)
+  self:set_origin(8, 17)
+
+  local sprite = self:get_sprite()
+  sprite:set_animation("immobilized")
+end
+
+function enemy:on_restarted()
+print('restarted')
+  vulnerable = false
+  for _, t in ipairs(timers) do t:stop() end
+  local sprite = self:get_sprite()
+
+  sprite:fade_out()
+  timers[#timers + 1] = sol.timer.start(self, 700, function() self:hide() end)
+end
+
+function enemy:hide()
+print('hiding')
+  vulnerable = false
+  ex, ey, el = self:get_position()
+  self:set_position(-100, -100)
+  timers[#timers + 1] = sol.timer.start(self, 500, function() self:unhide() end)
+end
+
+function enemy:unhide()
+print('unhiding')
+  vulnerable = true
+  self:set_position(ex, ey)
+  local sprite = self:get_sprite()
+  sprite:fade_in()
+  timers[#timers + 1] = sol.timer.start(self, 1000, function() self:fire() end)
+end
+
+function enemy:fire()
+print('firing')
+  vulnerable = true
+  beam = self:create_enemy({
+    breed = "wizzrobe_beam",
+    direction = self:set_sprite():get_direction()
+  })
+  self:restart()
+end
--- data/maps/215.lua
+++ data/maps/215.lua
@@ -6,9 +6,19 @@ local game = map:get_game()
 ---------------------------------------------
 
 function map:on_started(destination)
-
+  block_1:set_enabled(false)
+  block_2:set_enabled(false)
+  switch_2:set_enabled(false) -- re-enabled after Grim Creeper defeat
 end
 
 function sensor_hole:on_activated()
   map:get_hero():teleport("214", from_hole_above)
+end
+
+function switch_1:on_activated()
+  block_1:set_enabled(true)
+end
+
+function switch_2:on_actived()
+  block_2:set_enabled(true)
 end
