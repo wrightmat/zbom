9adadb1f73646a36db4ed2b96059864ed57a16c5
wrightmat <wright.matt.a@gmail.com>
2015-07-28 21:31:19 -0500

    Continuing work of adding enemies

--- data/enemies/knight_black.lua
+++ data/enemies/knight_black.lua
@@ -5,7 +5,6 @@ local enemy = ...
 sol.main.load_file("enemies/generic_leader")(enemy)
 enemy:set_properties({
   main_sprite = "enemies/knight_black",
-  sword_sprite = "enemies/knight_black_sword",
   life = 8,
   damage = 4,
   play_hero_seen_sound = true
