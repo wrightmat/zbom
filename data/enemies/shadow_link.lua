adfcfde14341f6ad2a4e9cd28da768c045340239
wrightmat <wright.matt.a@gmail.com>
2015-08-12 17:49:08 -0500

    A lot of various map changes. Mostly North Hyrule.

---
+++ data/enemies/shadow_link.lua
@@ -0,0 +1,20 @@
+local enemy = ...
+local map = enemy:get_map()
+
+-- Shadow Link
+
+function enemy:on_created()
+  self:set_life(10)
+  self:set_damage(8)
+  self:create_sprite("enemies/shadow_link")
+  self:set_size(32, 40)
+  self:set_origin(16, 36)
+  self:set_hurt_style("boss")
+  self:set_attack_consequence("arrow", "protected")
+  self:set_attack_consequence("hookshot", "protected")
+  self:set_attack_consequence("boomerang", "protected")
+  self:set_attack_consequence("explosion", "ignored")
+  self:set_attack_consequence("sword", "custom")
+  self:set_pushed_back_when_hurt(false)
+  self:set_push_hero_on_sword(false)
+end
--- data/items/hammer.lua
+++ data/items/hammer.lua
@@ -2,8 +2,11 @@ local item = ...
 
 function item:on_created()
   self:set_savegame_variable("i1839")
+  self:set_assignable(true)
 end
 
-function item:on_variant_changed(variant)
-
+function item:on_npc_interaction(npc)
+  if npc:get_name():find("^stake^") then
+    npc:set_enabled(false)
+  end
 end
