8a036503b4fe0883eabfbb4bdebe372a3042d8b2
Matt <wright.matt.a@gmail.com>
2015-08-19 17:58:14 -0500

    Improvements to ice cane - can turn water to ice
    Still some issues with collision tests, and there appears to be a bug
    related to that in Solarus 1.4.3

--- data/entities/ice_block.lua
+++ data/entities/ice_block.lua
@@ -10,10 +10,9 @@ function entity:on_created()
   self:set_size(16, 16)
   self:snap_to_grid()
   self:set_modified_ground("ice")
-  self:set_can_traverse("wall", false)
-  self:set_can_traverse("dynamic_tile", true)
   self:set_traversable_by("hero", false)
   self:set_traversable_by("custom_entity", true) --to allow pushing block into pit
+  self:set_can_traverse("dynamic_tile", true) --bug in Solarus 1.4.3 prevents this from working, so can't push block into lava/water
   self:create_sprite("entities/ice_block")
 
   self:add_collision_test("facing", function(self, other)
@@ -34,28 +33,38 @@ function entity:on_created()
   end)
 
   self:add_collision_test("overlapping", function(self, other)
-    local crust, switch
+    local lava_crust, ice_patch
     if other:get_type() == "dynamic_tile" then
-      local lpx, lpy, lpl = other:get_position()
-      local lsx, lsy, lsl = other:get_size()
-      self:clear_collision_tests()
+      local tsx, tsy = other:get_size()
+      local tpx, tpy, tpl = other:get_position()
       local sx, sy, sl = self:get_position()
+      self:clear_collision_tests()
       self:remove()
-      if (sy < lpy + 32 or sy > lpy - 32) then
-	if map:get_hero():get_direction() == 3 then
-          crust = map:create_dynamic_tile({x=sx-16,y=sy,layer=sl,width=32,height=32,pattern="lava_crust",enabled_at_start=true})
-	elseif map:get_hero():get_direction() == 1 then
-          crust = map:create_dynamic_tile({x=sx-16,y=sy-48,layer=sl,width=32,height=32,pattern="lava_crust",enabled_at_start=true})
-	elseif map:get_hero():get_direction() == 0 then
-          crust = map:create_dynamic_tile({x=sx+8,y=sy-16,layer=sl,width=32,height=32,pattern="lava_crust",enabled_at_start=true})
-	elseif map:get_hero():get_direction() == 2 then
-          crust = map:create_dynamic_tile({x=sx-40,y=sy-16,layer=sl,width=32,height=32,pattern="lava_crust",enabled_at_start=true})
-	end
-        crust:snap_to_grid()
-        sol.audio.play_sound("freeze")
-	sol.timer.start(map, 15000, function()
-	  crust:remove()
-	end)
+      if map:get_ground(sx,sy,sl) == "lava" then
+        if (sx > tpx-32) and (sx < tpx+tsx+32) and (sy > tpy-32) and (sy < tpy+tsy+32) then
+          lava_crust = map:create_custom_entity({ x = sx, y = sy, layer = sl, width = 32, height = 32, direction = 0 })
+          lava_crust:snap_to_grid()
+          sol.audio.play_sound("freeze")
+          lava_crust:create_sprite("entities/lava")
+          lava_crust:set_modified_ground("traversable")
+          lava_crust:set_traversable_by("hero", true)
+          lava_crust:set_traversable_by("enemy", true)
+	  sol.timer.start(map, 15000, function()
+	    lava_crust:remove()
+	  end)
+        end
+      elseif map:get_ground(sx,sy,sl) == "deep_water" then
+        if (sx > tpx-16) and (sx < tpx+tsx+16) and (sy > tpy-16) and (sy < tpy+tsy+16) then
+          ice_patch = map:create_custom_entity({ x = sx, y = sy, layer = sl, width = 32, height = 32, direction = 0 })
+          sol.audio.play_sound("freeze")
+          ice_patch:create_sprite("entities/ice")
+          ice_patch:set_modified_ground("ice")
+          ice_patch:set_traversable_by("hero", true)
+          ice_patch:set_traversable_by("enemy", true)
+	  sol.timer.start(map, 15000, function()
+	    ice_patch:remove()
+	  end)
+        end
       end
     elseif other:get_type() == "switch" then
       block_on_switch = true
