b6766c840ef51562398062eaf65bfe05c2246a92
Matt <wright.matt.a@gmail.com>
2015-08-27 07:27:18 -0500

    Working on Sanctum basement
    Need to figure out how to light multiple rooms at once, since overlay
    method probably won't work. Shaders?

--- data/maps/219.lua
+++ data/maps/219.lua
@@ -6,5 +6,32 @@ local game = map:get_game()
 ----------------------------------------------
 
 function map:on_started(destination)
+   game:set_value("dungeon_8_room_b25_lit")
+end
 
+for enemy in map:get_entities("room_1") do
+  enemy.on_dead = function()
+    if not map:has_entities("room_1") and not game:get_value("dungeon_8_room_b1_lit") then
+      sol.audio.play_sound("lamp")
+      game:set_value("dungeon_8_room_b1_lit")
+    end
+  end
+end
+
+for enemy in map:get_entities("room_12") do
+  enemy.on_dead = function()
+    if not map:has_entities("room_12") and not game:get_value("dungeon_8_room_b12_lit") then
+      sol.audio.play_sound("lamp")
+      game:set_value("dungeon_8_room_b12_lit")
+    end
+  end
+end
+
+for enemy in map:get_entities("room_14") do
+  enemy.on_dead = function()
+    if not map:has_entities("room_14") and not game:get_value("dungeon_8_room_b14_lit") then
+      sol.audio.play_sound("lamp")
+      game:set_value("dungeon_8_room_b14_lit")
+    end
+  end
 end
