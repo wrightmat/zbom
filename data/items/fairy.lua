a802b6bb0b95f129f927ce574f85696e293a54f2
Matt <wright.matt.a@gmail.com>
2015-10-29 21:27:33 -0500

    <HTML><META HTTP-EQUIV="content-type" CONTENT="text/html;charset=utf-8">Make fairies play a sound when restoring life even when already full

--- data/items/fairy.lua
+++ data/items/fairy.lua
@@ -1,4 +1,5 @@
 local item = ...
+local game = item:get_game() 
 
 -- This script defines the behavior of pickable fairies present on the map.
 
@@ -10,8 +11,8 @@ end
 
 -- A fairy appears on the map: create its movement.
 function item:on_pickable_created(pickable)
-   -- Create a movement that goes into random directions,
-   -- with a speed of 28 pixels per second.
+  -- Create a movement that goes into random directions,
+  -- with a speed of 28 pixels per second.
   local movement = sol.movement.create("random")
   movement:set_speed(28)
   movement:set_ignore_obstacles(true)
@@ -23,7 +24,7 @@ function item:on_pickable_created(pickable)
   pickable:set_layer_independent_collisions(true)  -- But detect collisions with lower layers anyway
 
   -- When the direction of the movement changes,
-  -- update the direction of the fairy's sprite
+  -- update the direction of the fairy's sprite.
   function pickable:on_movement_changed(movement)
     if pickable:get_followed_entity() == nil then
       local sprite = pickable:get_sprite()
@@ -40,25 +41,31 @@ end
 
 -- Obtaining a fairy.
 function item:on_obtaining(variant, savegame_variable)
-  local first_empty_bottle = self:get_game():get_first_empty_bottle()
-  if not self:get_game():has_bottle() or first_empty_bottle == nil then
+  local first_empty_bottle = game:get_first_empty_bottle()
+  if not game:has_bottle() or first_empty_bottle == nil then
     -- The player has no bottle: just restore 7 hearts.
-    self:get_game():add_life(7 * 4)
-    self:get_game():add_stamina(50)
+    game:add_life(7 * 4)
+    game:add_stamina(50)
   else
     -- The player has a bottle: start the dialog.
-    self:get_game():start_dialog("found_fairy", function(answer)
+    game:start_dialog("found_fairy", function(answer)
       if answer == "skipped" or answer == 1 then
-	-- Restore 7 hearts.
-	self:get_game():add_life(7 * 4)
-        self:get_game():add_stamina(20)
+        if game:get_life() == game:get_max_life() then  
+          -- Life is already full: we still play a sound to indicate that the
+          -- fairy was picked. Otherwise it looks like if nothing happened.  
+          sol.audio.play_sound("picked_item")  
+        else  
+          -- Restore 7 hearts.
+          game:add_life(7 * 4)
+          game:add_stamina(20)
+        end
       else
 	-- Keep the fairy in a bottle.
 	if first_empty_bottle == nil then
 	  -- No empty bottle.
-	  self:get_game():start_dialog("found_fairy.no_empty_bottle", function()
-	    self:get_game():add_life(7 * 4)
-    	    self:get_game():add_stamina(20)
+	  game:start_dialog("found_fairy.no_empty_bottle", function()
+	    game:add_life(7 * 4)
+    	    game:add_stamina(20)
 	  end)
 	  sol.audio.play_sound("wrong")
 	else
