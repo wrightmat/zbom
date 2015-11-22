47ecd596efbcdad3ca79913fac1296fab0404e5b
Matt <wright.matt.a@gmail.com>
2015-11-22 10:26:33 -0600

    Creating palaces in North Hyrule and setting Gibdos aflame

--- data/enemies/gibdos.lua
+++ data/enemies/gibdos.lua
@@ -15,5 +15,12 @@ behavior:create(enemy, properties)
 
 enemy:set_attack_arrow("protected")
 enemy:set_attack_hookshot("protected")
+enemy:set_attack_consequence("fire", "custom)
 enemy:set_attack_consequence("boomerang", "protected")
 enemy:set_attack_consequence("thrown_item", "protected")
+
+enemy:on_custom_attack_received(attack, sprite)
+  if attack == "fire" then enemy:get_sprite():set_animation("fire") end
+  enemy:hurt(3)
+  enemy:remove_life(3)
+end
