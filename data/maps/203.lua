df36a607bf41b492e184e6a4bef24f1286dd4654
Matt <wright.matt.a@gmail.com>
2015-12-12 10:15:25 -0600

    Disallow the player from getting Sacred Grove's book page again.
    Fix #28

--- data/maps/203.lua
+++ data/maps/203.lua
@@ -30,6 +30,9 @@ function map:on_started(destination)
   else
     map:set_doors_open("room6_shutter")
   end
+  -- Don't allow to get the page again (which is possible since it's taken away by the monkey).
+  -- Going back and getting it again forces the player to go back to the Pyramid to retrieve parts as well.
+  if game:get_value("i1807") >= 1 then chest_book:remove() end
 end
 
 function sensor_open_room18:on_activated()
