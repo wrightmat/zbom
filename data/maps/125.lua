1d867c257cf215532f1d2dd352dd4ddc9154ec60
Matt <wright.matt.a@gmail.com>
2015-12-12 18:37:16 -0600

    Make third sword work

--- data/maps/125.lua
+++ data/maps/125.lua
@@ -1,20 +1,6 @@
 local map = ...
 local game = map:get_game()
 
--------------------------------------------------------------------
--- Outside World K2 (Darunia Town/Goron Market) - Goron Market   --
--------------------------------------------------------------------
-
-local function random_walk_slow(npc)
-  local m = sol.movement.create("random_path")
-  m:set_speed(16)
-  m:start(npc)
-  npc:get_sprite():set_animation("walking")
-end
-
-function map:on_started()
-  random_walk_slow(goron_1)
-  random_walk_slow(goron_2)
-  random_walk_slow(goron_3)
-  random_walk_slow(goron_4)
-end
+-------------------------------------------
+-- Outside World K2 (Great Fairy Palace) --
+-------------------------------------------
--- data/maps/129.lua
+++ data/maps/129.lua
@@ -1,1 +1,17 @@
 local map = ...
+
+--------------------------------------------
+-- Outside World L1 (Darunia Town/Market) --
+--------------------------------------------
+
+local function random_walk_slow(npc)
+  local m = sol.movement.create("random_path")
+  m:set_speed(16)
+  m:start(npc)
+  npc:get_sprite():set_animation("walking")
+end
+
+function map:on_started()
+  random_walk_slow(goron_2)
+  random_walk_slow(goron_3)
+end
