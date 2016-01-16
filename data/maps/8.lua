cdcf2d5198b7ce549ad8c4c2bbd5ea5bc109ec41
Matt <wright.matt.a@gmail.com>
2016-01-15 19:03:09 -0600

    Implement ability to start book fetch quest

--- data/maps/8.lua
+++ data/maps/8.lua
@@ -33,38 +33,48 @@ function map:on_started(destination)
 end
 
 function npc_isan:on_interaction()
-  game:set_dialog_style("default")
-    if game:get_value("b2031") then
-      game:start_dialog("isan.0.trading", function(answer)
-        if answer == 1 then
-          -- give him the fish, get the cookbook
-          game:start_dialog("isan.0.trading_yes", function()
-            hero:start_treasure("trading", 12)
-            game:set_value("b2032", true)
-            game:set_value("b2031", false)
-            quest_trading_book:remove()
-          end)
-        else
-          -- don't give him the fish
-          game:start_dialog("isan.0.trading_no")
-        end
-      end)
-    elseif game:get_value("i1912") == 2 then
-      repeat -- make sure the same quote is not picked again
-        index = math.random(3)
-      until index ~= last_message
-      game:start_dialog("isan.2.library."..index)
-      last_message = index
-    elseif game:get_value("i1912") == 1 then
-      game:start_dialog("isan.1.library", function()
-        game:set_value("i1912", game:get_value("i1912")+1)
+  if game:get_value("b2031") then
+    game:start_dialog("isan.0.trading", function(answer)
+      if answer == 1 then
+        -- Give him the fish, get the cookbook.
+        game:start_dialog("isan.0.trading_yes", function()
+          hero:start_treasure("trading", 12)
+          game:set_value("b2032", true)
+          game:set_value("b2031", false)
+          quest_trading_book:remove()
+        end)
+      else
+        -- Don't give him the fish.
+        game:start_dialog("isan.0.trading_no")
+      end
+    end)
+  elseif game:get_value("i1615") >= 1 and game:get_value("b1614") then
+    if game:get_value("i1615") == 12 then
+      game:start_dialog("isan.library.2_done", function()
+        game:set_value("b1614", false)
+        map:get_hero():start_treasure("rupees", 5)
       end)
     else
-      game:start_dialog("isan.0.library", function()
-        game:set_value("i1032", 1)
-        game:set_value("i1912", game:get_value("i1912")+1)
-      end)
+      game:start_dialog("isan.library.2_count")
     end
+  elseif game:get_value("i1912") == 2 then
+    repeat -- Make sure the same quote is not picked again.
+      index = math.random(3)
+    until index ~= last_message
+    game:start_dialog("isan.2.library."..index)
+    -- Initiate the book fetch quest after it's been referenced.
+    if index == 2 then game:set_value("b1614", true) end
+    last_message = index
+  elseif game:get_value("i1912") == 1 then
+    game:start_dialog("isan.1.library", function()
+      game:set_value("i1912", game:get_value("i1912")+1)
+    end)
+  else
+    game:start_dialog("isan.0.library", function()
+      game:set_value("i1032", 1)
+      game:set_value("i1912", game:get_value("i1912")+1)
+    end)
+  end
 end
 
 function npc_saria_witch:on_interaction()
