1e0cf3e68c5a6ff1503e335849def19e8c3dc039
Matt <wright.matt.a@gmail.com>
2016-02-18 07:30:58 -0600

    Start working in the Gerutones

--- data/maps/135.lua
+++ data/maps/135.lua
@@ -1,1 +1,42 @@
 local map = ...
+local band_playing = false
+
+function map:on_started()
+  npc_gruce:get_sprite():set_animation("guitar")
+  npc_band_guitar:get_sprite():set_animation("guitar")
+end
+
+function sensor_band_1:on_activated()
+  local first_volume = 100
+  local second_volume = 1
+
+  if not band_playing then
+    --game:start_dialog("gruce")
+    npc_gruce:get_sprite():set_animation("strum")
+    sol.audio.play_music("gerutones", function()
+      npc_gruce:get_sprite():set_animation("guitar")
+      sol.audio.play_music("north_hyrule")
+    end)
+    sol.timer.start(self, 100, function() npc_gruce:get_sprite():set_animation("strumming") end)
+  else
+    npc_gruce:get_sprite():set_animation("guitar")
+    sol.timer.start(map, 50, function()
+      sol.audio.set_music_volume(first_volume)
+      first_volume = first_volume - 1  -- Fade music out by decreasing volume slowly (this is for the song).
+      if first_volume == 1 then return false else return true end
+    end)
+    sol.audio.play_music("north_hyrule")
+    sol.timer.start(map, 50, function()
+      sol.audio.set_music_volume(second_volume)
+      second_volume = second_volume + 1  -- Fade music in by increasing volume slowly (this is for the map music).
+      if second_volume >= 100 then return false else return true end
+    end)
+  end
+end
+
+function sensor_band_2:on_activated()
+  sensor_band_1:on_activated()
+end
+function sensor_band_3:on_activated()
+  sensor_band_1:on_activated()
+end
--- data/maps/41.lua
+++ data/maps/41.lua
@@ -6,37 +6,62 @@ local game = map:get_game()
 --------------------------------------
 
 function map:on_started(destination)
+  npc_marryn:get_sprite():set_animation("singing")
   local entrance_names = { "ranch" }
   for _, entrance_name in ipairs(entrance_names) do
     local sensor = map:get_entity(entrance_name .. "_door_sensor")
     local tile = map:get_entity(entrance_name .. "_door")
     sensor.on_activated_repeat = function()
-      if hero:get_direction() == 1
-	  and tile:is_enabled() then
-	tile:set_enabled(false)
-	sol.audio.play_sound("door_open")
+      if hero:get_direction() == 1 and tile:is_enabled() then
+        tile:set_enabled(false)
+        sol.audio.play_sound("door_open")
       end
     end
   end
-
   -- Activate any night-specific dynamic tiles.
   if game:get_time_of_day() == "night" then
     for entity in game:get_map():get_entities("night_") do
       entity:set_enabled(true)
     end
   end
-
-  npc_marryn:get_sprite():set_animation("singing")
 end
 
 function npc_marryn:on_interaction()
+  local first_volume = 100
+  local second_volume = 1
   game:start_dialog("marryn.0.ranch", function()
+    sol.timer.start(map, 50, function()
+      sol.audio.set_music_volume(first_volume)
+      first_volume = first_volume - 1  -- Fade music out by decreasing volume slowly (this is for the ranch).
+      if first_volume == 1 then return false else return true end
+    end)
     sol.audio.play_music("ballad", function()
+      sol.timer.start(map, 50, function()
+        sol.audio.set_music_volume(first_volume)
+        first_volume = first_volume - 1  -- Fade music out by decreasing volume slowly (this is for the ballad).
+        if first_volume == 1 then return false else return true end
+      end)
       sol.audio.play_music("ranch")
+      sol.timer.start(map, 50, function()
+        sol.audio.set_music_volume(second_volume)
+        second_volume = second_volume + 1  -- Fade music in by increasing volume slowly (this is for the ranch).
+        if second_volume >= 100 then return false else return true end
+      end)
+    end)
+    sol.timer.start(map, 50, function()
+      sol.audio.set_music_volume(second_volume)
+      second_volume = second_volume + 1  -- Fade music in by increasing volume slowly (this is for the ballad).
+      if second_volume >= 100 then return false else return true end
     end)
   end)
 end
 
 function sensor_singing:on_left()
+  local volume = 1
   sol.audio.play_music("ranch")
+  sol.timer.start(map, 50, function()
+    sol.audio.set_music_volume(volume)
+    volume = volume + 1  -- Fade music in by increasing volume slowly (this is for the ranch).
+    if volume >= 100 then return false else return true end
+  end)
 end
