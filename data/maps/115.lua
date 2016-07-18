899c6118e297c615e6d9412882256ebb9aae691d
Matt <wright.matt.a@gmail.com>
2016-07-18 17:41:14 -0500

    Mido and Darunia respond to day/night

--- data/maps/115.lua
+++ data/maps/115.lua
@@ -1,1 +1,10 @@
 local map = ...
+
+function map:on_started(destination)
+  -- Activate any night-specific dynamic tiles.
+  if game:get_time_of_day() == "night" then
+    for entity in game:get_map():get_entities("night_") do
+      entity:set_enabled(true)
+    end
+  end
+end
