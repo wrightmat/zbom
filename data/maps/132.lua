62f2ba2df5fc1c562cab0bedf87d958a13c80ba4
Matt <wright.matt.a@gmail.com>
2016-08-17 21:55:39 -0500

    Animals

--- data/maps/132.lua
+++ data/maps/132.lua
@@ -1,1 +1,18 @@
 local map = ...
+local game = map:get_game()
+
+----------------------------------------
+-- Outside World L4 (Old Kasuto Town) --
+----------------------------------------
+
+local function random_walk(npc)
+  local m = sol.movement.create("random_path")
+  m:set_speed(32)
+  m:start(npc)
+  npc:get_sprite():set_animation("walking")
+end
+
+function map:on_started()
+  random_walk(npc_squirrel_1)
+  random_walk(npc_squirrel_2)
+end
