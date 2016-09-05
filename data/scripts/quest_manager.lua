49605e862461aee45f556204cce345dd9e473bff
Matt <wright.matt.a@gmail.com>
2016-09-05 10:20:40 -0500

    Implement custom move_camera function
    Borrowed from christopho's version in zsdx

--- data/scripts/quest_manager.lua
+++ data/scripts/quest_manager.lua
@@ -428,6 +428,47 @@ local function initialize_maps()
       shadow:draw_region(camera_x,camera_y,camera_w,camera_h,dst_surface)
     end
     if screen_overlay ~= nil then screen_overlay:draw(dst_surface) end
+
+    -- As of Solarus 1.5, the function map:move_camera is deprecated, so we use a custom version instead.
+    function map_meta:move_camera(x, y, speed, callback, delay_before, delay_after)
+      local camera = self:get_camera()
+      local game = self:get_game()
+      local hero = self:get_hero()
+      
+      delay_before = delay_before or 1000
+      delay_after = delay_after or 1000
+      
+      local back_x, back_y = camera:get_position_to_track(hero)
+      game:set_suspended(true)
+      camera:start_manual()
+
+      local movement = sol.movement.create("target")
+      movement:set_target(camera:get_position_to_track(x, y))
+      movement:set_ignore_obstacles(true)
+      movement:set_speed(speed)
+      movement:start(camera, function()
+        local timer_1 = sol.timer.start(self, delay_before, function()
+          if callback ~= nil then
+            callback()
+          end
+          local timer_2 = sol.timer.start(self, delay_after, function()
+            local movement = sol.movement.create("target")
+            movement:set_target(back_x, back_y)
+            movement:set_ignore_obstacles(true)
+            movement:set_speed(speed)
+            movement:start(camera, function()
+              game:set_suspended(false)
+              camera:start_tracking(hero)
+              if self.on_camera_back ~= nil then
+                self:on_camera_back()
+              end
+            end)
+          end)
+          timer_2:set_suspended_with_map(false)
+        end)
+        timer_1:set_suspended_with_map(false)
+      end)
+    end
   end
   
   function map_metatable:on_started(destination)
