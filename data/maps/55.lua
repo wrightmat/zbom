ec626b5c7a1d6548c6f421c95500a5e88b58cde9
Matt <wright.matt.a@gmail.com>
2017-01-12 20:31:29 -0600

    Tweaks to the Septen Height area

--- data/maps/55.lua
+++ data/maps/55.lua
@@ -48,17 +48,3 @@ function map:on_started(destination)
     end
   end
 end
-
-function sensor_change_layer:on_activated()
-  -- If walking forward on low level, change to intermediate level.
-  if layer ~= 1 and hero:get_direction() == 1 then
-    local x, y, layer = hero:get_position()
-    hero:set_position(x, y, layer+1)
-  end
-
-  -- If walking down on intermediate level, changle to low level.
-  if layer ~= 0 and hero:get_direction() == 3 then
-    local x, y, layer = hero:get_position()
-    hero:set_position(x, y, layer-1)
-  end
-end
