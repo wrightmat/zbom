faf36c99ddae965c42c956db54f3a8056f105777
Matt <wright.matt.a@gmail.com>
2017-04-07 08:18:37 -0500

    Corrections to Ruins script. Fix #85

--- data/maps/201.lua
+++ data/maps/201.lua
@@ -5,6 +5,8 @@ local game = map:get_game()
 -- Dungeon 1: Eastern Ruins --
 ------------------------------
 
+if game:get_value("i1808") == nil then game:set_value("i1808", 0) end
+
 function map:on_started(destination)
   game:set_value("b1034", false)
   game:set_value("b1046", false)
@@ -14,7 +16,7 @@ function map:on_started(destination)
   if not game:get_value("b1035") then boss_heart:set_enabled(false) end
   if game:get_value("b1050") then map:open_doors("door_miniboss") end
   if game:get_value("b1787") then chest_treasure:set_enabled(false) end
-  if game:get_value("b1195") or game:get_value("i1808") > 0 then chest_big:set_open(true) end
+  if game:get_value("b1195") then chest_big:set_open(true) end
 end
 
 function map:on_update()
