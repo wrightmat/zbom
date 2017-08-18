2431f6a3d71e60c1d09ff8fb4bff6610cfe1b599
Matt <wright.matt.a@gmail.com>
2017-08-17 21:58:04 -0500

    Fix quicksand

--- data/entities/quicksand.lua
+++ data/entities/quicksand.lua
@@ -16,11 +16,18 @@ function entity:on_created()
   self:add_collision_test("overlapping", function(quicksand, other)
     if other:get_type() ~= "hero" then return end
     local hero = other
+    local hx, hy, hl = hero:get_position()
+    -- Make sure the hero doesn't get stuck in the quicksand forever. Set the return point to just
+    -- left of the quicksand unless that's an obstacle, then do just to the right.
+    if not hero:test_obstacles(-24, 0) then
+      hero:save_solid_ground(ex - 24, ey, el)
+    else
+      hero:save_solid_ground(ex + 24, ey, el)
+    end
     -- Move the hero toward the quicksand's center every 20 ms while he is overlapping it (and not in a state like "running").
     if timer == nil and (hero:get_state() == "free" or hero:get_state() == "sword loading") then
       timer = sol.timer.start(self, 20, function()
         time_in = time_in + 1
-        hx, hy, hl = hero:get_position()
         if ex > hx then hx = hx + 2 else hx = hx - 2 end
         if ey > hy then hy = hy + 2 else hy = hy - 2 end
         hero:set_position(hx, hy)
