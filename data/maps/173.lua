043805083a5ad9127dfad3745d745facc5be6eb4
Matt <wright.matt.a@gmail.com>
2019-07-12 17:30:49 -0500

    Makes changes in swamp temple more apparent. Fix #120

--- data/maps/173.lua
+++ data/maps/173.lua
@@ -22,30 +22,32 @@ function flying_heart_2:on_obtained()
 end
 
 function switch_1:on_activated()
-  map:open_doors("door_shutter")
-  game:set_value("b1222", true)
+  map:move_camera(232, 568, 250, function()
+    map:open_doors("door_shutter")
+    game:set_value("b1222", true)
+  end, 500, 500)
 end
 
 function switch_2:on_activated()
   sol.audio.play_sound("secret")
-  -- Drain swamp water to expose spoils chest.
-  map:set_entities_enabled("swamp_drain", true)
-  swamp_water_1:fade_out(100, function()
+  map:move_camera(112, 824, 250, function()
+    -- Drain swamp water to expose spoils chest.
+    map:set_entities_enabled("swamp_drain", true)
     barrier:set_enabled(false)
     swamp_water_1:set_enabled(false)
     sol.timer.start(map, 1000, function() map:set_entities_enabled("swamp_drain", false) end)
-    sol.timer.start(map, 20000, function() map:revert_swamp_water() end)  -- Water comes back after 20 seconds.
-  end)
+    sol.timer.start(map, 60000, function() map:revert_swamp_water() end)  -- Water comes back after 60 seconds.
+  end, 500, 500)
 end
 
 function map:revert_swamp_water()
-  -- Drain swamp water to expose spoils chest.
-  map:set_entities_enabled("swamp_drain", true)
-  swamp_water_1:get_sprite():fade_in(100, function()
+  -- Replace drained water
+  map:move_camera(112, 824, 350, function()
+    map:set_entities_enabled("swamp_drain", true)
     barrier:set_enabled(true)
     swamp_water_1:set_enabled(true)
     sol.timer.start(map, 1000, function() map:set_entities_enabled("swamp_drain", false) end)
-  end)
+  end, 500, 500)
 end
 
 for enemy in map:get_entities("flower") do
